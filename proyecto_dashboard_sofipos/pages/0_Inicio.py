7968d35aae21d22ee74132707314477943a2eb36
Leonardo RR <40375889+lrebollar@users.noreply.github.com>
2026-08-31 17:17:49 -0600

    a

--- proyecto_dashboard_sofipos/pages/0_Inicio.py
+++ proyecto_dashboard_sofipos/pages/0_Inicio.py
@@ -3,8 +3,9 @@ import streamlit as st
 st.title("Dashboard Sector SOFIPOS - Fincomún")
 st.write("Selecciona una sección para comenzar:")
 
-st.page_link("pages/1_Cartera_de_credito.py", label="📈 Información de Cartera de Crédito", icon="📈")
-st.page_link("pages/2_EPRC.py", label="Estimaciones Preventivas de Riesgo Crediticio")
+st.page_link("pages/1_Benchmark SOFIPOS - Fincomún.py", label="📈 Benchmark SOFIPOS - Fincomún", icon="📈")
+st.page_link("pages/2_Cartera_de_credito.py", label="📈 Información de Cartera de Crédito", icon="📈")
+st.page_link("pages/4_EPRC.py", label="Estimaciones Preventivas de Riesgo Crediticio")
 st.page_link("pages/3_Captacion.py", label="Captación tradicional")
-st.page_link("pages/4_Ingresos_por_intereses.py", label="Ingresos por intereses")
+st.page_link("pages/6_Ingresos_por_intereses.py", label="Ingresos por intereses")
 st.page_link("pages/5_Indicadores_financieros.py", label="Indicadores Financieros")
